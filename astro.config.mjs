c869326f08bcfb4caf78e70c20b6458804aaa992
DreamerOnACloud <dreamer.cloud.dreamer@gmail.com>
2024-09-26 12:20:37 +0200

    move all astro content to root dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  root: './astro', // Add this line to set the default root directory
   integrations: [tailwind()]
 });
