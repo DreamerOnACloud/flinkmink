68e04bbdc379611e17c6978d07e59c4a623e1e16
DreamerOnACloud <dreamer.cloud.dreamer@gmail.com>
2024-09-30 12:19:06 +0200

    add: projects content

--- src/content/config.ts
+++ src/content/config.ts
@@ -7,10 +7,22 @@ const postsCollection = defineCollection({
         title: z.string(),
         date: z.string(),
         image: image(),
+        description: z.string(),
+    })
+})
+
+const projectsCollection = defineCollection({
+    schema: ({image}) => z.object({ // 'image()' is imported implicitly and available the moment we define 'schema' as a (arrow) function
+        title: z.string(),
+        image: image(),
+        url: z.string(),
+        description: z.string(),
+        tags: z.array(z.string()),
     })
 })
 
 // Includes all of the collections defined above - key name should match dir, e.g. "posts"
 export const collections = {
-    posts: postsCollection
+    posts: postsCollection,
+    projects: projectsCollection
 }
---
+++ src/content/projects/cook-smart-ai.md
@@ -0,0 +1,8 @@
+---
+title: "Cook smart AI"
+image: "./images/cook-smart-ai.png"
+url: "https://knorgi-fight-game.netlify.app/"
+description: "An AI cooking assistant to help you come up with recipes based on your personal needs."
+tags: ["AI", "cooking", "web"]
+---
+
